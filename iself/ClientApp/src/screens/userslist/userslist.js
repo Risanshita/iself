085a76375125a8fdd7139c3d996b3df4b84c9e84
Nitish Roy <nitishr833@gmail.com>
2022-08-14 22:29:45 +0530

    Action button

--- iself/ClientApp/src/screens/userslist/userslist.js
+++ iself/ClientApp/src/screens/userslist/userslist.js
@@ -2,15 +2,31 @@ import { Col, Row, Space, Table, Tag } from "antd";
 import React, { useContext, useState } from "react";
 import { AccountContext } from "../../context/accountContext";
 import ListHeader from "./../../components/ListHeader";
-
+import { Dropdown, Menu } from "antd";
+import { DownOutlined } from "@ant-design/icons";
 import "./Style.css";
 
 const UsersList = () => {
   const [isInitialLoad, setInitialLoad] = useState(true);
   const [users, setUsers] = useState([]);
-
+  // const [color, setColor] = useState("green");
   const { actions } = useContext(AccountContext);
   const { userList } = actions.account;
+
+  const enterLoading = (index) => {
+    setLoadings((state) => {
+      const newLoadings = [...state];
+      newLoadings[index] = true;
+      return newLoadings;
+    });
+    setTimeout(() => {
+      setLoadings((state) => {
+        const newLoadings = [...state];
+        newLoadings[index] = false;
+        return newLoadings;
+      });
+    }, 6000);
+  };
 
   if (isInitialLoad) {
     userList("", (a) => {
@@ -23,9 +39,32 @@ const UsersList = () => {
       setUsers(a);
     });
   };
+
+  const menu = (
+    <Menu
+      items={[
+        {
+          label: "Reset",
+          key: "1",
+        },
+        {
+          label: "Delete",
+          key: "2",
+        },
+        {
+          label: "Activate",
+          key: "3",
+        },
+        {
+          label: "Deactivate",
+          key: "4",
+        },
+      ]}
+    />
+  );
+
   const columns = [
     {
-      profile: "",
       title: "Full Name",
       dataIndex: "fullName",
       key: "name",
@@ -57,8 +96,14 @@ const UsersList = () => {
       key: "action",
       render: (_, record) => (
         <Space size="middle">
-          <a>Reset Password</a>
-          <a>Delete</a>
+          <Dropdown.Button
+            icon={<DownOutlined />}
+            overlay={menu}
+            trigger={["click"]}
+            onClick={(e) => e.preventDefault()}
+          >
+            Action
+          </Dropdown.Button>
         </Space>
       ),
     },
@@ -138,6 +183,8 @@ const UsersList = () => {
       role: "User",
     },
   ];
+
+  const [loadings, setLoadings] = useState([]);
 
   return (
     <Row className="usermanage-page">
